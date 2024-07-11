a0b4655c467f2f4b5c58a8e53b3ac19708a6b17d
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-07-11 12:58:51 -0600

    feat: Update main.rs with lazy loading for polars dependency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-
+use polars::lazy::dsl::*;
 use polars::prelude::*;
 
+#[allow(dead_code)]
 fn change_level(level: Expr) -> Expr {
     level.map(
         |s: Series| -> PolarsResult<Option<Series>> {
@@ -16,101 +17,17 @@ fn change_level(level: Expr) -> Expr {
     )
 }
 
-fn main() {
-    let path = "/Users/armando/Downloads/Base de datos original 080724.csv";
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let path: &str = "/Users/armando/Downloads/Base de datos.csv";
 
-    // let df = CsvReadOptions::default()
-    //     .try_into_reader_with_file_path(Some(path.into()))
-    //     .unwrap()
-    //     .finish()
-    //     .unwrap();
+    let mut schema: Schema = Schema::new();
+    schema.with_column("registro".into(), DataType::UInt32);
 
-    let schema = Schema::new();
+    let df: LazyFrame = LazyCsvReader::new(path)
+    .with_dtype_overwrite(Some(Arc::new(schema)))
+    .finish()?;
 
-    let df: LazyFrame = LazyCsvReader::new(path).finish()?;
+    println!("{:?}", df.collect()?);
 
-    // let users = df
-    //     .clone()
-    //     .lazy()
-    //     .group_by([col("registro")])
-    //     .agg([
-    //          col("nombre_completo").first(),
-    //          col("tipo").first(),
-    //          col("estado").first(),
-    //          col("semestre").first(),
-    //          col("grupo").first(),
-    //          col("turno").first(),
-    //          change_level(col("nivel").first()).alias("nivel"),
-    //     ])
-    //     .sort(
-    //         ["registro"],
-    //         SortMultipleOptions::default()
-    //         )
-    //     .limit(5)
-    //     .collect()
-    //     .unwrap();
-
-    // println!("{:?}", users);
-
-    // let out = df
-    //     .clone()
-    //     .lazy()
-    //     .filter(col("registro").eq(lit(21110110)))
-    //     .group_by([col("registro")])
-    //     .agg([
-    //          len().alias("count"),
-    //          col("nombre_completo").first(),
-    //          col("especialidad").unique().first(),
-    //          col("clave").unique().alias("claves"),
-    //     ])
-    //     .sort(
-    //         ["registro"],
-    //         SortMultipleOptions::default()
-    //         .with_order_descending(false)
-    //         .with_nulls_last(true),
-    //         )
-    //     .limit(10)
-    //     .collect()
-    //     .unwrap();
-
-    // println!("{:?}", out);
-
-    // let subjects = df
-    //     .clone()
-    //     .lazy()
-    //     .group_by([col("clave")])
-    //     .agg([
-    //         col("nombre").unique().first()
-    //     ]
-    //     ).collect()
-    //     .unwrap();
-
-    // print!("{:?}", subjects);
-
-    // let divisions = df
-    //     .clone()
-    //     .lazy()
-    //     .group_by(["academia"])
-    //     .agg([
-    //          col("division").unique()
-    //     ]).collect()
-    //     .unwrap();
-
-    // println!("{}", divisions);
-
-    // let teachers = df
-    //     .clone()
-    //     .lazy()
-    //     .group_by(["nomina"])
-    //     .agg([
-    //          col("nombre").unique()
-    //     ])
-    //     .sort(
-    //         ["nomina"],
-    //         SortMultipleOptions::default()
-    //         )
-    //     .collect()
-    //     .unwrap();
-
-    // print!("{}", teachers)
+    Ok(())
 }
